940e7c76429955bc5305b0a7bb7929908644117a
ankit <ankit.srivastava@pickcel.com>
2018-05-05 22:18:22 +0530

    testing

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrent', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (test) => {
+    return test.toUpperCase();
+})
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now} ${req.method}  ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        console.log('Unable to fetch the content');
+    })
+    next();
+});
+app.use((req, res, next) => {
+    res.render('maintance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'HOME PAGE',
+        welcomeMessage: 'welcome to my website'
+
+    });
+    // res.send('Hello Ankit');
+});
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+
+
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log('server is now started at 3000');
+});  
